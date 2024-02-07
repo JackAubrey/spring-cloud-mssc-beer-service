72a4a06372c7ffce747d8f7713025b63fa7c7874
danilo <d.cividin@hotmail.it>
2024-02-07 14:52:51 +0100

    added JSON creation example

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -1,12 +1,25 @@
 package guru.springframework.msscbeerservice.web.model;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 public class BeerPagedList extends PageImpl<BeerDto> {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BeerPagedList(
+            @JsonProperty("content") List<BeerDto> content,
+            @JsonProperty("pageNumber") int pageNumber,
+            @JsonProperty("pageSize") int pageSize,
+            @JsonProperty("sort") Sort.Direction sort,
+            @JsonProperty("total") long total) {
+        super(content, PageRequest.of(pageNumber,pageSize, sort), total);
+    }
+
     public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
