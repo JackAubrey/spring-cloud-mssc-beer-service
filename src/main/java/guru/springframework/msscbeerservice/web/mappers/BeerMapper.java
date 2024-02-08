e402298c12100c7d3a0376438432b8e687e1f276
danilo <d.cividin@hotmail.it>
2024-02-08 18:49:49 +0100

    added support for Brewery Inventory Service

--- src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -3,16 +3,21 @@ package guru.springframework.msscbeerservice.web.mappers;
 import guru.springframework.msscbeerservice.domain.Beer;
 import guru.springframework.msscbeerservice.web.converters.DateConverter;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateConverter.class}, componentModel = "spring")
+@DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
     /*@Mappings(
             @Mapping(source = "minOnHand", target = "quantityOnHand")
     )*/
     @Mapping(target = "quantityOnHand", ignore = true)
     BeerDto beerToBeerDto(Beer beer);
+
+    @Mapping(target = "quantityOnHand", ignore = true)
+    BeerDto beerToBeerDtoWithInventory(Beer beer);
 
     @Mapping(target = "minOnHand", ignore = true)
     @Mapping(target = "quantityToBrew", ignore = true)
