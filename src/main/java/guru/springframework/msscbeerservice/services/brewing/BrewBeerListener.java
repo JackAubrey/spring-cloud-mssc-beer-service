19935d2e5f3fcd179277d2fc1b82aff14a8a712a
danilo <d.cividin@hotmail.it>
2024-02-21 00:10:23 +0100

    fix: used transactional annotation

--- src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -2,13 +2,13 @@ package guru.springframework.msscbeerservice.services.brewing;
 
 import guru.springframework.msscbeerservice.config.JmsConfig;
 import guru.springframework.msscbeerservice.domain.Beer;
-import guru.springframework.msscbeerservice.events.BrewBeerEvent;
-import guru.springframework.msscbeerservice.events.NewInventoryEvent;
+import guru.sfg.beer.common.events.BrewBeerEvent;
+import guru.sfg.beer.common.events.NewInventoryEvent;
 import guru.springframework.msscbeerservice.repositories.BeerRepository;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
@@ -16,11 +16,11 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-@Lazy
 public class BrewBeerListener {
     private final BeerRepository beerRepository;
     private final JmsTemplate jmsTemplate;
 
+    @Transactional
     @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
     void listen(BrewBeerEvent event) {
         BeerDto beerDto = event.getBeerDto();
@@ -28,7 +28,7 @@ public class BrewBeerListener {
 
         beerDto.setQuantityOnHand(beer.getQuantityToBrew());
 
-        log.info("Brewing Beer {} QQH: {}", beer.getBeerName(), beerDto.getQuantityOnHand());
+        log.info("Brewing Beer {} {} QOH: {}", beer.getBeerName(), beer.getId(), beerDto.getQuantityOnHand());
 
         NewInventoryEvent inventoryEvent = new NewInventoryEvent(beerDto);
         jmsTemplate.convertAndSend(JmsConfig.NEW_INVENTORY_QUEUE, inventoryEvent);
