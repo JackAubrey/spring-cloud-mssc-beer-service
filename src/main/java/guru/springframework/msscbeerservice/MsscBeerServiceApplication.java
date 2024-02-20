ae0662c3bb8964e1d8d8f4a82e9556d6c8ea7fd6
danilo <d.cividin@hotmail.it>
2024-02-21 00:11:19 +0100

    added scan packages

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -3,7 +3,7 @@ package guru.springframework.msscbeerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"guru.sfg.beer.common", "guru.springframework.msscbeerservice"})
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
